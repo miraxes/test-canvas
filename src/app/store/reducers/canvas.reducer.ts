dc3c673cddb441374d0550f55faba89353939426
miraxes <mr.miraxes@gmail.com>
2019-12-24 12:03:16 +0200

    aot stuff <3

--- src/app/store/reducers/canvas.reducer.ts
+++ src/app/store/reducers/canvas.reducer.ts
@@ -5,15 +5,15 @@ import { environment } from '../../../environments/environment';
 import { CanvasActionTypes, LayerUnion } from '../actions/canvas.actions';
 import { State } from '.';
 
-export const canvasReducer = (state = [], action: LayerUnion) => {
-     switch (action.type) {
-       case CanvasActionTypes.AddLayer:
-         return [...state, action.payload.textLayer];
-       case CanvasActionTypes.RemoveLastLayer:
-            return state.slice(0, -1);
-       default:
-         return state;
-     }
-    };
+export function canvasReducer(state = [], action: LayerUnion) {
+  switch (action.type) {
+    case CanvasActionTypes.AddLayer:
+      return [...state, action.payload.textLayer];
+    case CanvasActionTypes.RemoveLastLayer:
+        return state.slice(0, -1);
+    default:
+      return state;
+  }
+}
 
 export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
